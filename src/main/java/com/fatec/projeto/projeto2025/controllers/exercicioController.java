899236053b3a89dbda84f851706b58f16f8615e8
mariamorais18 <maria.morais14@fatec.sp.gov.br>
2025-02-20 22:46:34 -0300

    Atts final aula

--- src/main/java/com/fatec/projeto/projeto2025/controllers/exercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/exercicioController.java
@@ -1,6 +1,9 @@
 package com.fatec.projeto.projeto2025.controllers;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +12,11 @@ public class exercicioController {
     @GetMapping("")
     public String Helloworld(){
         return "Hello";
+    }
+
+
+    @GetMapping("{nome}")
+    public String Helloworld(@PathVariable Optional<String> nome){
+        return nome.isPresent() ? nome.get() : "dd";
     }
 }
