8c79dfa1ab3a4ddcecde07618b5f58dc96af04c1
mariamorais18 <maria.morais14@fatec.sp.gov.br>
2025-02-27 22:04:47 -0300

    Exercício Par ou Impar

--- src/main/java/com/fatec/projeto/projeto2025/controllers/exercicioController.java
+++ src/main/java/com/fatec/projeto/projeto2025/controllers/exercicioController.java
@@ -14,21 +14,44 @@ public class exercicioController {
         return "Hello";
     }
 
-
-    @GetMapping("{idade}")
-    public String verificaIdade(@PathVariable Optional<String> idade){
+   @GetMapping("/get-idade/{idade}")
+  public String verificaIdade(@PathVariable Optional<String> idade){
         idade.ifPresent(value -> System.out.println("Idade: " + value));
         int idadeInt = Integer.parseInt(idade.get());
         if (idadeInt > 0 && idadeInt <12){
             return "Criança";
         }else if (idadeInt >= 12 && idadeInt <= 18){
             return "Adolescente";
-        }else if (idadeInt >= 19 && idadeInt <= 60){
+       }else if (idadeInt >= 19 && idadeInt <= 60){
             return "Adulto";
-        }if (idadeInt > 60){
-            return "Idoso";
+       }if (idadeInt > 60){
+          return "Idoso";
         } else {
             return "Idade inválida";
         }
     }
+
+
+    @GetMapping("/get-num/{num}")
+    public String verificaParImpar(@PathVariable Optional<String> num){
+        num.ifPresent(value -> System.out.println("Numero: " + value));
+
+        try{
+        int numInt = Integer.parseInt(num.get());
+        if (numInt % 2 == 0 && numInt > 0){
+            return "Número é par";
+        }else if (numInt % 2 != 0 && numInt > 0){
+            return "Número é impar";
+        }else if (numInt == 0){
+            return "Número é 0";
+        } else {
+            return "Número inválido";
+        }
+    }catch(NumberFormatException e) {
+        return "O valor não é um número, informe novamente";
+    }
 }
+
+
+}
+
